7cbb2597654d91e31fc146a412cf4368faf5594e
jeremiah <egbeejay@gmail.com>
2017-10-25 10:15:34 +0500

    working login with api

--- js/Admin.js
+++ js/Admin.js
@@ -3,78 +3,99 @@ $('document').ready(function() {
 
 	/*student registration*/
 
-	$('#signin').click(function(e){
-       e.preventDefault()
+	// $('#signin').click(function(e){
+ //       e.preventDefault()
 
 
 
 
-       /*input your variable*/
-       var name = $('#name');
-       var email = $('#email');
-       var password = $('#password');
-       var accountType = $('#accountType');
+ //       /*input your variable*/
+ //       var name = $('#name');
+ //       var email = $('#email');
+ //       var password = $('#password');
+ //       var accountType = $('#accountType');
 
       
 
 
-       var student = {
-       	name: name.val(),
-       	email: email.val(),
-       	password: password.val(),
-       	accountType: accountType.val(),
-       }
+ //       var student = {
+ //       	name: name.val(),
+ //       	email: email.val(),
+ //       	password: password.val(),
+ //       	accountType: accountType.val(),
+ //       }
 
-       $.ajax({
-       	type: 'POST',
-       	url: 'http://18.220.175.109/account/register/admin',
-       	data: student,
-       	dataType: "JSON",
-       	success: function(){
-       		alert('sucessful registration');
-       	},
-         error:function(status){
-         	alert('error '+ status.responseText);
-         }
-       });
+ //       $.ajax({
+ //       	type: 'POST',
+ //       	url: 'http://18.220.175.109/account/register/admin',
+ //       	data: student,
+ //       	dataType: "JSON",
+ //       	success: function(){
+ //       		alert('sucessful registration');
+ //       	},
+ //         error:function(status){
+ //         	alert('error '+ status.responseText);
+ //         }
+ //       });
 
-	});
+	// });
 
 			
 				     
 				/*business registration*/
 
-					$('#submit').click(function(e){
-				       e.preventDefault()
+					// $('#submit').click(function(e){
+				 //       e.preventDefault()
 
 
-				       /*input your variable*/
-				       var name = $('#name');
-				       var email = $('#email');
-				       var password = $('#password');
-				       var businessType = $('#businessType');
+				 //       /*input your variable*/
+				 //       var name = $('#name');
+				 //       var email = $('#email');
+				 //       var password = $('#password');
+				 //       var businessType = $('#businessType');
 
-				       var business = {
-				       	name: name.val(),
-				       	email: email.val(),
-				       	password: password.val(),
-				       	businessType: businessType.val(),
-				       }
+				 //       var business = {
+				 //       	name: name.val(),
+				 //       	email: email.val(),
+				 //       	password: password.val(),
+				 //       	businessType: businessType.val(),
+				 //       }
 
-				       $.ajax({
-				       	type: 'POST',
-				       	url: 'http://18.220.175.109/account/register/business',
-				       	data: business,
-				       	dataType: "JSON",
-				       	success: function(){
-				       		alert('sucessful registration');
-				       	},
-				         error:function(status){
-				         	alert('error '+ status.responseText);
-				         }
-				       });
+				 //       $.ajax({
+				 //       	type: 'POST',
+				 //       	url: 'http://18.220.175.109/account/register/business',
+				 //       	data: business,
+				 //       	dataType: "JSON",
+				 //       	success: function(){
+				 //       		alert('sucessful registration');
+				 //       	},
+				 //         error:function(status){
+				 //         	alert('error '+ status.responseText);
+				 //         }
+				 //       });
 
-					});
+					// });
+
+
+
+
+
+					// Email validation
+
+					$('#email').focusout(function () {
+						    function isEmail(email) {
+						        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+						        return regex.test(email);
+						    }
+
+						    var emailinput = $(this).val();
+
+						    if (emailinput === "" || !(isEmail(emailinput))) {
+						        $('#invalid').css('display', 'block');
+						    }else{
+						    	$('#invalid').css('display', 'none')
+						    }
+						});
 				     
 
 				/*student login*/
@@ -91,8 +112,8 @@ $('document').ready(function() {
 
 				       var studentLogin = {
 				       	
-				       	email: email.val(),
-				       	password: password.val(),
+				       	email: email.val().trim(),
+				       	password: password.val().trim(),
 				       	
 				       }
 
@@ -103,10 +124,12 @@ $('document').ready(function() {
 				       	data: studentLogin,
 				       	dataType: "JSON",
 				       	success: function(){
-				       		alert('sucessful login');
+				       		// alert('sucessful login');
+				       		window.location= 'business.html';
 				       	},
 				         error:function(status){
-				         	alert('error '+ status.responseText);
+				         	$('#message').css('display', 'block'),
+				         	$('#message').append(status.responseJSON.error);
 				         }
 				       });
 
@@ -115,64 +138,64 @@ $('document').ready(function() {
 
                      /*forgotten password*/
                        
-                       $('#para').click(function(e){
-				       e.preventDefault()
+       //                 $('#para').click(function(e){
+				   //     e.preventDefault()
 				       
-				       var password ={
-				       	email:email.val(),
-				       }
+				   //     var password ={
+				   //     	email:email.val(),
+				   //     }
 
-				       $.ajax({
-				       	type: 'POST',
-				       	url: 'http://18.220.175.109/account/recovery/password',
-				       	data:password,
-				       	dataType: "JSON",
-				       	success: function(){
-				       		alert('success');
-				       	},
-				       	error:function(status){
-				       		alert('error ' + status.responseText);
-				       	}
-				       });
+				   //     $.ajax({
+				   //     	type: 'POST',
+				   //     	url: 'http://18.220.175.109/account/recovery/password',
+				   //     	data:password,
+				   //     	dataType: "JSON",
+				   //     	success: function(){
+				   //     		alert('success');
+				   //     	},
+				   //     	error:function(status){
+				   //     		alert('error ' + status.responseText);
+				   //     	}
+				   //     });
 
-				   });
+				   // });
 
                      
 
 				/*business login*/
 
-					$('#bus').click(function(e){
-				       e.preventDefault()
+					// $('#bus').click(function(e){
+				 //       e.preventDefault()
 
 
-				       /*input your variable*/
+				 //       /*input your variable*/
 				       
-				       var email = $('#email');
-				       var password = $('#password');
+				 //       var email = $('#email');
+				 //       var password = $('#password');
 				       
 
-				       var businessLogin = {
+				 //       var businessLogin = {
 				       	
-				       	email: email.val(),
-				       	password: password.val(),
+				 //       	email: email.val(),
+				 //       	password: password.val(),
 				       	
-				       }
+				 //       }
 
-				       $.ajax({
-				       	type: 'POST',
-				       	url: 'http://18.220.175.109/account/login',
-				       	data: businessLogin,
-				       	dataType: "JSON",
-				       	success: function(){
-				       		alert('sucessful login');
-				       	},
-				         error:function(status){
-				         	alert('error '+ status.responseText);
-				         }
-				       });
+				 //       $.ajax({
+				 //       	type: 'POST',
+				 //       	url: 'http://18.220.175.109/account/login',
+				 //       	data: businessLogin,
+				 //       	dataType: "JSON",
+				 //       	success: function(){
+				 //       		alert('sucessful login');
+				 //       	},
+				 //         error:function(status){
+				 //         	alert('error '+ status.responseText);
+				 //         }
+				 //       });
 
 				      
-					});
+					// });
 
 
 
